f74a548ea590eb214b02b61cc97d710e6d32605c
Adesegun Koiki <adesegun.koiki@sterling.ng>
2022-12-30 18:00:13 +0100

    django model done

---
+++ django_restframework/Readme.md
@@ -0,0 +1,14 @@
+"""Activate virtual env"""
+.\venv\Scripts\activate
+
+
+"""create app"""
+python manage.py startapp
+
+"""Stopped at """
+54:44
+
+"""migrations"""
+python manage.py makemigrations
+python manage.py migrate
+
---
+++ django_restframework/py_client/basic.py
@@ -0,0 +1,12 @@
+import requests
+
+# endpoint = "https://httpbin.org/status/200/"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/api/"
+
+get_response = requests.get(endpoint, json={"query":"Hello world"}) 
+# params={"abc":123}
+# print(get_response.text)
+
+print(get_response.json())
+# print(get_response.status_code)
